c6b195be3060c6440655440993a4125ebdda2fe3
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-10-28 13:14:33 +0200

    cart functional

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,14 @@
 import classes from './CartItem.module.css';
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch();
+
+  const removeCartItemHandler = () => {
+    dispatch(cartActions.removeFromCart({ id }));
+  }
 
   return (
     <li className={classes.item}>
@@ -17,7 +24,7 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
+          <button onClick={removeCartItemHandler}>-</button>
           <button>+</button>
         </div>
       </div>
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,11 +2,13 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
+          id={2}
           title='Test'
           price={6}
           description='This is a first product - amazing!'
