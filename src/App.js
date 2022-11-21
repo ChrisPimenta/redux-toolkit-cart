0ab245cac0550b06f938a74b7ff532fe7435068b
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-11-21 20:34:26 +0100

    Send cart data to firebase db on change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import Cart from './components/Cart/Cart';
+import { useEffect } from 'react';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
 
 function App() {
   const isCartShown = useSelector(state => state.ui.isCartShown);
+
+  const cart = useSelector(state => state.cart);
+
+  // Whenever cart changes send http request.
+  useEffect(() => {
+    fetch('https://react-http2-f5ae2-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+      method: 'put',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      //make sure to serialize your JSON body
+      body: JSON.stringify(cart)
+    })
+  }, [cart])
 
   return (
     <Layout>
